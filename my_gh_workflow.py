7dc6b14218b2d998d6f59b2ed3c511dc3499534c
wangyuxin <wangyuxin@mokahr.com>
2024-10-22 16:41:31 +0800

    add

---
+++ my_gh_workflow.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+import httpx
+from prefect import flow, task
+
+
+@task(retries=2)
+def get_repo_info(repo_owner: str, repo_name: str) -> dict[str, Any]:
+    """Get info about a repo - will retry twice after failing"""
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}"
+    api_response = httpx.get(url)
+    api_response.raise_for_status()
+    repo_info = api_response.json()
+    return repo_info
+
+
+@task
+def get_contributors(repo_info: dict) -> list[dict[str, Any]]:
+    """Get contributors for a repo"""
+    contributors_url = repo_info["contributors_url"]
+    response = httpx.get(contributors_url)
+    response.raise_for_status()
+    contributors = response.json()
+    return contributors
+
+
+@flow(log_prints=True)
+def log_repo_info(repo_owner: str = "wangyuxinwhy", repo_name: str = "uniem"):
+    """
+    Given a GitHub repository, logs the number of stargazers
+    and contributors for that repo.
+    """
+    repo_info = get_repo_info(repo_owner, repo_name)
+    print(f"Stars 🌠 : {repo_info['stargazers_count']}")
+
+    contributors = get_contributors(repo_info)
+    print(f"Number of contributors 👷: {len(contributors)}")
+
+
+if __name__ == "__main__":
+    log_repo_info()
